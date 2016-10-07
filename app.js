2b78f5e301b57566f6293c6da8ae300bae4d4135
Jerry Tan <jerrytansk@gmail.com>
2016-10-08 03:16:14 +0800

    return json response

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-var express = require('express');
+"use strict!"
+
+var express = require('express')
 var app = express();
+var bodyParser = require('body-parser')
+
+app.use( bodyParser.json() )       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}))
+
+app.use(express.json())       // to support JSON-encoded bodies
+app.use(express.urlencoded()) // to support URL-encoded bodies
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
+})
 
 app.post('/fitchen/event/load', function(req, res){
-	console.log(req.body);
-	res.send('Received!')
-});
+	res.setHeader('Content-Type', 'application/json')
+	console.log(req.body)
+	res.json({status: 'Received!'})
+})
 
 app.listen(5000, function () {
-  console.log('Fitchen webhooks listening on port 5000!');
-});
+  console.log('Fitchen webhooks listening on port 5000!')
+})
