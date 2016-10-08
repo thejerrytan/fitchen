4095745cb4abb19d9ba5da0406a627dffa9c6d1d
Jerry Tan <jerrytansk@gmail.com>
2016-10-09 02:25:44 +0800

    next

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(function (req, res, next) {
   express.static(path.join(__dirname, '/public'))
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
 })
 app.set('views', './views')
 app.set('view engine', 'jade')
